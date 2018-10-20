0f3aacf0ed5ae00b17eeb96246aca30811de0941
Jacob Walters <walters.610@osu.edu>
2018-10-20 08:30:15 -0400

    user update

--- js/user.js
+++ js/user.js
@@ -1,12 +1,3 @@
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('../db/refugeedb.db', (err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQlite database.');
-  });
-
-
 function insertUser(){
     var email = document.getElementById('email').value;
     var first = document .getElementById('first').value;
@@ -19,34 +10,11 @@ function insertUser(){
     var platforms = document.getElementById('platforms').value;
     var expertise = document.getElementById('expertise').value;
     var password = document.getElementById('password').value;
-
-    var query = "INSERT INTO users (email, first_n, last_n, phone, lang, platform, password, expertise) VALUES (email, first, last, phone, langs, platform, password, expertise)";
-    db.run(query, function(err){
-        if(err) throw err;
-    });
-}
-
-
-function matchingFunction(expertise, language){
-    var matches = [];
-    var query = "SELECT * FROM users WHERE expertise = " + expertise + "OR lang = " + language;
-    db.all(query, function(err, rows){
-        rows.forEach(function(row){
-            matches.push(row);
-        })
-    });
-    return matches;
-}
-
-function queryUserData(){
-    var query = "SELECT expertise, language FROM users WHERE id = " + id;
-    var matched = [];
-    db.all(query, function(err, rows){
-        rows.forEach(function(row){
-            matched.push(matchingFunction(row['expertise'], row['lang']));
-        })
-    });
-    return matched;
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    xmlhttp.open("POST", "/json-handler");
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({email:email, first:first, last:last, phone:phone, langs:langs, platforms:platforms, expertise:expertise, password:password}));
+    insertUser_serv(email, first, last, phone, city, state, country, langs, platforms, expertise, password);
 }
 
 
